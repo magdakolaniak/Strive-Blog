92f57789f7df3f078bb358cc4b28960420341d07
magdakolaniak <magda.kolaniak@gmail.com>
2021-05-26 16:51:46 +0200

    fixing-build

---
+++ build/precache-manifest.db360a2b69bee81a61b0ce139c1298c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "18fc028c892fd5ba428c",
+    "url": "/strive-blog-template/static/css/main.f9a7f1d4.chunk.css"
+  },
+  {
+    "revision": "18fc028c892fd5ba428c",
+    "url": "/strive-blog-template/static/js/main.e5a34ab5.chunk.js"
+  },
+  {
+    "revision": "6eaf45a51ecb2c859b1d",
+    "url": "/strive-blog-template/static/js/runtime~main.9811295d.js"
+  },
+  {
+    "revision": "05d33fb88f9708725906",
+    "url": "/strive-blog-template/static/css/2.54cf461e.chunk.css"
+  },
+  {
+    "revision": "05d33fb88f9708725906",
+    "url": "/strive-blog-template/static/js/2.ad667d17.chunk.js"
+  },
+  {
+    "revision": "229c911164a70bb280ec",
+    "url": "/strive-blog-template/static/js/3.fd81210f.chunk.js"
+  },
+  {
+    "revision": "8aef1d57f79ff07fc3a3d48c34451535",
+    "url": "/strive-blog-template/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/strive-blog-template/precache-manifest.db360a2b69bee81a61b0ce139c1298c4.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/strive-blog-template/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
